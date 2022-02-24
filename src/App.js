58a42652a38ccbcbb0436ceac0ad12a39f9ce335
Yongrok <94chl@naver.com>
2022-02-24 23:38:41 +0900

    test: 빌드 테스트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { MainPage, NotFoundPage, ResultPage, SubmittedePage } from "@/pages";
 import GlobalStyle from "@/styles/GlobalStyle";
 import { Container } from "@/styles/Container";
@@ -8,14 +8,14 @@ function App() {
   return (
     <Container>
       <GlobalStyle />
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<MainPage />}></Route>
           <Route path="/result/:id" element={<ResultPage />}></Route>
           <Route path="/submitted/:id" element={<SubmittedePage />}></Route>
           <Route path="/*" element={<NotFoundPage />}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </Container>
   );
 }
